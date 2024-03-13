1e0d800d87e7a17c103d4b99dbf4b7984fb448f1
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-13 06:35:28 +0530

    1. Check Number is Negative or Positive

--- src/Basic_Math_Questions/PositiveAndNegative.java
+++ src/Basic_Math_Questions/PositiveAndNegative.java
@@ -1,7 +1,22 @@
 package Basic_Math_Questions;
 
+import java.util.Scanner;
+
 public class PositiveAndNegative {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Positive and Negative Number Question is Here");
+
+        System.out.print("Enter Number : - ");
+        int num = sc.nextInt();
+
+        if(num > 0) {
+            System.out.println("Number is Positive ");
+        }else if( num == 0){
+            System.out.println("Number is Zero");
+        }else{
+            System.out.println("Number is Negative");
+        }
+
     }
 }
