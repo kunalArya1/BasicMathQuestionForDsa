c493d6dccd2354040bd6e866cc047214256def6d
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-13 06:27:33 +0530

    Basic Math Question for Dsa Repo

---
+++ src/Basic_Math_Questions/PositiveAndNegative.java
@@ -0,0 +1,7 @@
+package Basic_Math_Questions;
+
+public class PositiveAndNegative {
+    public static void main(String[] args) {
+        System.out.println("Positive and Negative Number Question is Here");
+    }
+}
